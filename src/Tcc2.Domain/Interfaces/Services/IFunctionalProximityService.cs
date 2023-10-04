c2b1f65ea1b6f2fdd5a603a80cb10e9062dee80e
mauriciogomes02 <mauricio.gomes@warren.com.br>
2023-10-03 21:39:51 -0300

    add functional proximity service

---
+++ src/Tcc2.Domain/Interfaces/Services/IFunctionalProximityService.cs
@@ -0,0 +1,13 @@
+﻿using Tcc2.Domain.Entities;
+using Tcc2.Domain.Pagination;
+
+namespace Tcc2.Domain.Interfaces.Services;
+
+public interface IFunctionalProximityService
+{
+    Task<Paginated<Person>> GetAsync(
+        Activity activity,
+        int pageIndex,
+        int pageSize,
+        CancellationToken cancellationToken);
+}
---
+++ src/Tcc2.Domain/Services/FunctionalProximityService.cs
@@ -0,0 +1,32 @@
+﻿using Tcc2.Domain.Entities;
+using Tcc2.Domain.Interfaces.Infrastructure.Repositories;
+using Tcc2.Domain.Interfaces.Services;
+using Tcc2.Domain.Pagination;
+
+namespace Tcc2.Domain.Services;
+
+public class FunctionalProximityService : IFunctionalProximityService
+{
+    private readonly IPersonRepository _personRepository;
+
+    public FunctionalProximityService(IPersonRepository personRepository)
+    {
+        _personRepository = personRepository;
+    }
+
+    public Task<Paginated<Person>> GetAsync(
+        Activity activity,
+        int pageIndex,
+        int pageSize,
+        CancellationToken cancellationToken)
+    {
+        var criteria = new Criteria<Person, Person>(
+            x => x.Activities != null
+                && x.Activities.Any(y => y.Date == activity.Date),
+            x => x,
+            pageIndex,
+            pageSize);
+
+        return _personRepository.GetAsync(criteria, cancellationToken);
+    }
+}
